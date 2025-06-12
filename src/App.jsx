c0220dddc8140b672b4e8bd8cf9542bb8e1d5a86
Tom Naor <tom@getjurni.ai>
2025-06-12 09:59:46 +0300

    initialzie

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="text-3xl font-bold underline">Hey</div>;
-}
-
-export default App;
